f5fda143f5254d548b22741cde8255f15a47ec44
Geng-Wung <gengxis92@gmail.com>
2022-08-30 10:28:18 -0400

    commit tree 746f1760d8114c4fcf6ec7af7a5520d2a14b42a3

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push("Ralph");
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift("Bob");
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+function appendCat(name) {
+  const copycats = cats.slice();
+    copycats.push("Broom");
+    return copycats;
+}
+
+function prependCat(name) {
+    const copycats = cats.slice();
+    copycats.unshift("Arnold");
+    return copycats;
+}
+
+function removeLastCat() {
+    const copycats = cats.slice();
+    copycats.pop();
+    return copycats;
+}
+
+function removeFirstCat() {
+    const copycats = cats.slice();
+    copycats.shift();
+    return copycats;
+}
